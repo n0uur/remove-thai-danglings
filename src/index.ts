0af52c3b485755dd66bb2523e3c19798e44a725a
Krittanut Siripornnoppakhun <50010805+n0uur@users.noreply.github.com>
2024-12-04 21:29:01 +0700

    feat: working main function

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+/**
+ * Remove Thai non-base characters at the beginning of text.
+ *
+ * All of this code is based on PyThaiNLP project in Python. so all credits go to them.
+ *
+ * This is a common "typo", especially for input field in a form,
+ * as these non-base characters can be visually hidden from user
+ * who may accidentally type them in.
+ *
+ * A character to be removed should be both:
+ *   - tone mark, above vowel, below vowel, or non-base sign AND
+ *   - located at the beginning of the text
+ * and also remove zero width characters at the end of text.
+ *
+ * @param {string} text - input text
+ * @returns {string} text without dangling Thai characters at the beginning
+ */
+function removeThaiDangling(text: string): string {
+  const thaiAboveVowels = "\u0e31\u0e34\u0e35\u0e36\u0e37\u0e4d\u0e47";
+  const thaiBelowVowels = "\u0e38\u0e39";
+  const thaiTonemarks = "\u0e48\u0e49\u0e4a\u0e4b";
+  const zeroWidthChars = "\u200b\u200c\u200d";
+
+  const danglingChars = `${thaiAboveVowels}${thaiBelowVowels}${thaiTonemarks}\u0e3a\u0e4c\u0e4d\u0e4e${zeroWidthChars}`;
+  const reRemoveDanglings = new RegExp(`^[${danglingChars}]+`); // remove dangling characters at the beginning
+  const reRemoveZeroWidthChars = new RegExp(`${zeroWidthChars}+$`); // remove zero width characters at the end
+
+  return text.replace(reRemoveDanglings, "").replace(reRemoveZeroWidthChars, "");
+}
+
+export default removeThaiDangling;
